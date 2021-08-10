08150daccb6690afd03c67039ed25bfdc7de7e19
Zeon <zeonstewart23@gmail.com>
2021-08-10 17:14:33 -0400

    commit tree 69816c39ccb35a811cf3642b3ae79d272e9f51e5

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function razzle() {
+    console.log("You've been razzled!");
+}
+razzle();
+
+function razzle(lawyer = "Billy", target = " 'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}!`);
+}
+razzle();
+razzle("Methuselah", "T'challah");
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const fn = function() {
+    console.log("Yet more razzing");
+}
+fn;
+
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(adj = "*") {
+    function newAdj(innerAdj = "special") {
+        return `You are ${adj}${innerAdj}${adj}!`
+    }
+    return newAdj;
+}
+
+wrapAdjective("%")("a dedicated programmer");
